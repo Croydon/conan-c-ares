d97996a57da6eeb7063e18a37f6c0db086e8c258
Luis Herrero Corralo <luis.herrero@gmail.com>
2017-02-08 12:48:51 +0100

    module build script (Visual Studio 2013)

---
+++ build.py
@@ -0,0 +1,15 @@
+from conan.packager import ConanMultiPackager
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(username="demo")
+    # Try shared and not-shared builds
+    builder.add_common_builds(shared_option_name="c-ares:shared", pure_c=True)
+	
+	# Filter
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["compiler.version"] == "12":
+            filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
+    
+    builder.run()
