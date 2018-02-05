9b304cc317875fdb9a5e64785d6db7e2a02c64f4
Croydon <cr0ydon@outlook.com>
2018-02-05 02:08:36 +0100

    Remove obsolete CMake() calls

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,8 +12,8 @@ class caresReuseConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        cmake = CMake(self)
+        self.run('cmake "%s" %s' % (self.build_folder, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
